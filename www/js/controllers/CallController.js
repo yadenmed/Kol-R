b6743919072b1509c60ca8b60e9f8ee27138c805
YADEN MED <yadenmed@gmail.com>
2016-03-17 16:06:51 +0100

    first-commit

---
+++ www/js/controllers/CallController.js
@@ -0,0 +1,120 @@
+(function(){
+	angular.module('starter')
+	.controller('CallController', ['localStorageService', '$scope', '$ionicPopup','$ionicLoading', CallController]);
+	
+	function CallController(localStorageService, $scope, $ionicPopup){
+
+			$scope.username = localStorageService.get('username');
+			$scope.currentCall = false;
+
+
+			var peer = new Peer($scope.username, {
+				key: 'dhcycgs877t4kj4i',
+				config: {'iceServers': [
+		            { url: 'stun:stun1.l.google.com:19302' },
+		            { url: 'turn:numb.viagenie.ca', credential: 'muazkh', username: 'webrtc@live.com' }
+        		]}
+			});
+			// var peer = new Peer($scope.username, {
+			//         host: '127.0.0.1', port: 3000, path: '/peerjs',
+			//         config: {'iceServers': [
+			//             { url: 'stun:stun1.l.google.com:19302' },
+			//             { url: 'turn:numb.viagenie.ca', credential: 'muazkh', username: 'webrtc@live.com' }
+			//         ]}
+			//     }
+			// );
+
+			function getVideo(successCallback, errorCallback){
+			    navigator.webkitGetUserMedia({audio: true, video: true}, successCallback, errorCallback);
+			}
+
+
+			function onReceiveCall(call){
+				$scope.currentCall = call;
+				$ionicPopup.confirm({
+					title: 'Incoming Call',
+					template: call.peer+'is calling you. '
+				})
+				.then(function(res) {
+				     if(res) {
+				           getVideo(
+				               function(MediaStream){
+				               		
+				                   call.answer(MediaStream);
+				                   var video = document.getElementById('local-video');
+				                   video.src = window.URL.createObjectURL(MediaStream);
+				               },
+				               function(err){
+				               
+				                   $ionicPopup.alert({
+				       	        	title: 'Error',
+				       	        	template: 'An error occured while try to connect to the device mic and camera'
+				       	        });
+				               }
+				           );
+				     } else {
+				       call.close();
+				     }
+				   });
+
+			    call.on('stream', onReceiveStream);
+			}
+
+
+			function onReceiveStream(stream){
+				$ionicLoading.hide();
+			    var video = document.getElementById('contact-video');
+			    video.src = window.URL.createObjectURL(stream);
+			    video.onloadedmetadata = function(){
+					$ionicPopup.alert({
+						title: 'Call Ongoing',
+						template: 'Call has started. You can speak now'
+					});
+			    };
+
+			}
+			$scope.endCall = function(){
+				$scope.currentCall.close();
+				$scope.currentCall = null;
+			}
+			$scope.startCall = function(){
+				var contact_username = $scope.contact_username;
+		
+				getVideo(
+				    function(MediaStream){
+
+				        $scope.currentCall = peer.call(contact_username, MediaStream);
+				        
+				       	var video = document.getElementById('local-video');
+				       	video.src = window.URL.createObjectURL(MediaStream);
+				        	    
+				        $ionicLoading.show({
+				             template: 'In progress...'
+				           });
+
+				        $scope.currentCall.on('stream', onReceiveStream);
+				        $scope.currentCall.on('close',function(){
+				        $scope.currentCall = null;
+				        $ionicPopup.alert({
+				        	title: 'Call Denied',
+				        	template: 'Your call was denied !'
+				        });
+				    });
+				    },
+				    function(err){
+				        $ionicPopup.alert({
+				        	title: 'Error',
+				        	template: 'An error occured while try to connect to the device mic and camera'
+				        });
+				    }
+				);
+
+			};
+
+			peer.on('call', onReceiveCall);
+
+
+
+	}
+
+})();
